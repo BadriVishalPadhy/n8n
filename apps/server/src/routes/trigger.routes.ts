d0a79c33c2792531eefd97eab03d0a339687a4c7
Badri Vishal Padhy <badrivishalpadhy@gmail.com>
2026-01-04 15:07:22 +0530

    Feat:action and trigger routes added

--- apps/server/src/routes/trigger.routes.ts
+++ apps/server/src/routes/trigger.routes.ts
@@ -1,16 +1,13 @@
 import { Router } from "express";
 import { prismaClient } from "@repo/db";
-const triggerRouter:Router = Router();
 
+const triggerRouter: Router = Router();
 
-triggerRouter.get('/', async(req,res) => {
-    const triggers = await prismaClient.availableTriggerNodes.findMany({});
-     res.json({
-      triggers
-     })
-     console.log("triggers",triggers)
-
-} )
-
+triggerRouter.get("/", async (req, res) => {
+  const triggers = await prismaClient.availableTriggerNodes.findMany({});
+  res.json({
+    triggers,
+  });
+});
 
 export default triggerRouter;
